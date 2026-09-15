4eb3761679b21ac9478d491f16c3eb13f1b50d75
TimaFrolov <tima1911@yandex.ru>
2026-09-15 19:40:45 +0300

    chore(nix): switch to modern flake-compat approach

--- shell.nix
+++ shell.nix
@@ -1,16 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-  shared  = import ./nix/shared.nix { inherit pkgs; };
-  uvm-src = import ./nix/uvm.nix    { inherit pkgs; };
-in
-pkgs.mkShell {
-  packages =
-    shared.nativeBuildInputs
-    ++ shared.buildInputs
-    ++ shared.shellOnlyPackages;
-
-  shellHook = ''
-    export SURELOG_ROOT=${pkgs.surelog}
-    export VERIHOGG_UVM_PATH=${uvm-src}/src
-  '';
-}
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      nodeName = lock.nodes.root.inputs.flake-compat;
+    in
+    fetchTarball {
+      url = lock.nodes.${nodeName}.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.${nodeName}.locked.rev}.tar.gz";
+      sha256 = lock.nodes.${nodeName}.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
